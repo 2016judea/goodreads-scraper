7c8f0cc69076bf288b559417f69a7a7cf9ec9cb8
Aidan Jude <judea@amazon.com>
2021-12-24 11:46:28 -0600

    Fix shelf merging encoding issues

--- shelves_merger.py
+++ shelves_merger.py
@@ -1,8 +1,9 @@
-from glob import glob
-import json
-from termcolor import colored
 import argparse
+import json
 from functools import reduce
+from glob import glob
+
+from termcolor import colored
 from tqdm import tqdm
 
 SHELVES_PAGES_DIR = "./shelves_pages"
@@ -74,20 +75,20 @@ class ShelvesMerger():
         print(colored("Dumping authors...", 'yellow'))
         authors = sorted(set(map(lambda b: b["author"], self.books)))
         print(colored("{} unique authors in total!".format(len(authors)), 'green', attrs=["bold"]))
-        with open("_data/authors.json", "w") as f:
+        with open("_data/authors.json", "w", encoding="utf-8") as f:
             json.dump({"authors": authors}, f, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False)
 
     def dump_genres(self):
         print(colored("Dumping genres...", 'yellow'))
         genres = sorted(reduce(lambda r, c: r.union(c), map(lambda b: set(b["genres"]), self.books)))
         print(colored("{} unique genres in total!".format(len(genres)), 'green', attrs=["bold"]))
-        with open("_data/genres.json", "w") as f:
+        with open("_data/genres.json", "w", encoding="utf-8") as f:
             json.dump({"genres": genres}, f, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False)
 
     def dump_reviews(self):
         print(colored("Dumping reviews...", 'yellow'))
         reviews = {b["goodreads_url"]: b["reviews"] for b in self.books}
-        with open("_data/reviews.json", "w") as f:
+        with open("_data/reviews.json", "w", encoding="utf-8") as f:
             json.dump({"reviews": reviews}, f, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False)
         print(colored("Saved all books reviews!", 'green', attrs=["bold"]))
 
@@ -95,7 +96,7 @@ class ShelvesMerger():
         print(colored("Dumping books...", 'yellow'))
         for b in self.books:
             del b["reviews"]
-        with open("_data/books.json", "w") as f:
+        with open("_data/books.json", "w", encoding="utf-8") as f:
             json.dump({"books": self.books}, f, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False)
         print(colored("Saved all {} books!".format(len(self.books)), 'green', attrs=["bold"]))
 
